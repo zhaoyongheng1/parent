b646e8be924be91a724a53397b5012a3397e2a94
ly <598048929@qq.com>
2016-11-25 21:23:17 +0800

    增加oauth2认证1

--- zuul/src/main/java/cn/waicaibao/ZuulApplication.java
+++ zuul/src/main/java/cn/waicaibao/ZuulApplication.java
@@ -1,11 +1,8 @@
 package cn.waicaibao;
 
-import cn.waicaibao.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringCloudApplication
@@ -20,5 +17,16 @@ public class ZuulApplication {
 //	@Bean
 //	public AccessFilter accessFilter() {
 //		return new AccessFilter();
+//	}
+//	@Configuration
+//	@Order(-20)
+//	protected static class LoginConfig extends WebSecurityConfigurerAdapter {
+//
+//		@Override
+//		protected void configure(HttpSecurity http) throws Exception {
+//			http.authorizeRequests().antMatchers("/uaa/**").permitAll();
+//		}
+//
+//
 //	}
 }
