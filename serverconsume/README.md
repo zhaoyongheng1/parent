aaf4a5b0de91f7d0f73c6e63dcd1c1f4756cbae3
ly <598048929@qq.com>
2017-11-28 19:11:26 +0800

    1、serverconsume 2、serverprovide

---
+++ serverconsume/README.md
@@ -0,0 +1,5 @@
+#spring cloud项目学习
+
+服务消费者
+
+http://127.0.0.1:5001/hystrix.stream
--- serverprovide/src/main/java/cn/com.myproject/ServerProvideApplication.java
+++ serverprovide/src/main/java/cn/com.myproject/ServerProvideApplication.java
@@ -1,14 +1,10 @@
 package cn.com.myproject;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringCloudApplication
 public class ServerProvideApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerProvideApplication.class, args);
 	}
